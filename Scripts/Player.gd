1834fe7f9c443b6dc31e8b99e82a897ea96cee08
Amithabh-A <112101004@smail.iitpkd.ac.in>
2024-02-15 22:12:01 +0530

    Added Day 3 files.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -17,4 +17,12 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -JUMP_SPEED
 	velocity.y += GRAVITY
+	
+	_animate()
 	move_and_slide()
+
+func _animate():
+	var asss = $AnimatedSprite2D
+	if is_on_floor():
+		asss.play("idle")
+	
